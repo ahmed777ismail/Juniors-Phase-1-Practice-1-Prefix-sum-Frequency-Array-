a8cc825ac7fc2cb44af40bac7dc32a46e6baf54e
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2025-02-21 08:08:29 +0200

    Add files via upload
    A. Pangram

---
+++ A. Pangram/A. Pangram/A. Pangram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    set<char> letters;
+
+    for (char c : s) {
+        letters.insert(tolower(c)); // Convert to lowercase and insert into set
+    }
+
+    if (letters.size() == 26) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
